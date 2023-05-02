a8dfac19171f184277a84f059a1eb74df2437c28
ashwin <telmoreashwin2001@gmail.com>
2023-05-03 00:49:39 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,76 @@
+const API_BASE_URL = "https://api.videosdk.live";
+// const VIDEOSDK_TOKEN = process.env.REACT_APP_VIDEOSDK_TOKEN;
+// const VIDEOSDK_TOKEN = "​​eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0NDZkNjNmYy1mYmRkLTRhNWMtOTdmYy02M2JjMzIwZDlhNzMiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY3OTIyOTM1NywiZXhwIjoxNjc5ODM0MTU3fQ.AElgf8voCbSMVPnRYxC8MeBoqPrm68-bVFezr_-ChqU";
+const API_AUTH_URL = process.env;
+const VIDEOSDK_TOKEN = process.env.REACT_APP_VIDEOSDK_TOKEN;
+
+console.log('first', VIDEOSDK_TOKEN)
+export const getToken = async () => {
+  return VIDEOSDK_TOKEN
+  if (VIDEOSDK_TOKEN && API_AUTH_URL) {
+    console.error(
+      "Error: Provide only ONE PARAMETER - either Token or Auth API"
+    );
+  } else if (VIDEOSDK_TOKEN) {
+    return VIDEOSDK_TOKEN;
+  } else if (API_AUTH_URL) {
+    const res = await fetch(`${API_AUTH_URL}/get-token`, {
+      method: "GET",
+    });
+    const { token } = await res.json();
+    return token;
+  } else {
+    console.error("Error: ", Error("Please add a token or Auth Server URL"));
+  }
+};
+
+export const createMeeting = async ({ token }) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+  //Destructuring the roomId from the response
+  console.log('res :>> ', res);
+  const { roomId } = await res.json();
+  console.log('roomId :>> ', roomId);
+  return roomId;
+};
+// export const createMeeting = async ({ token }) => {
+//   console.log('token', token)
+//   const url = `${API_BASE_URL}/v2/rooms`;
+//   const options = {
+//     method: "POST",
+//     headers: { 
+//       authorization: `${token}`, 
+//       "Content-Type": "application/json" 
+//     },
+//     body: JSON.stringify({}),
+//   };
+
+//   const { roomId } = await fetch(url, options)
+//     .then((response) => response.json())
+//     .catch((error) => console.error("error", error));
+
+//   console.log('roomId :>> ', roomId);
+
+//   return roomId;
+// };
+
+export const validateMeeting = async ({ roomId, token }) => {
+  const url = `${API_BASE_URL}/v2/rooms/validate/${roomId}`;
+
+  const options = {
+    method: "GET",
+    headers: { Authorization: token, "Content-Type": "application/json" },
+  };
+
+  const result = await fetch(url, options)
+    .then((response) => response.json()) //result will have meeting id
+    .catch((error) => console.error("error", error));
+
+  return result ? result.roomId === roomId : false;
+};
---
+++ src/meeting/components/ParticipantView.js
@@ -0,0 +1,74 @@
+import React, { useMemo } from "react";
+import { useMeeting } from "@videosdk.live/react-sdk";
+import { MemoizedParticipantGrid } from "../../components/ParticipantGrid";
+
+function ParticipantsViewer({ isPresenting }) {
+  const {
+    participants,
+    pinnedParticipants,
+    activeSpeakerId,
+    localParticipant,
+    localScreenShareOn,
+    presenterId,
+  } = useMeeting();
+
+  const participantIds = useMemo(() => {
+    console.log('useMeeting', {
+      participants,
+      pinnedParticipants,
+      activeSpeakerId,
+      localParticipant,
+      localScreenShareOn,
+      presenterId,
+    })
+    const pinnedParticipantId = [...pinnedParticipants.keys()].filter(
+      (participantId) => {
+        return participantId != localParticipant.id;
+      }
+    );
+    const regularParticipantIds = [...participants.keys()].filter(
+      (participantId) => {
+        return (
+          ![...pinnedParticipants.keys()].includes(participantId) &&
+          localParticipant.id != participantId
+        );
+      }
+    );
+
+    const ids = [
+      localParticipant.id,
+      ...pinnedParticipantId,
+      ...regularParticipantIds,
+    ].slice(0, isPresenting ? 6 : 16);
+    console.log('ids :>> ', ids);
+
+    if (activeSpeakerId) {
+      if (!ids.includes(activeSpeakerId)) {
+        ids[ids.length - 1] = activeSpeakerId;
+      }
+    }
+    return ids;
+  }, [
+    participants,
+    activeSpeakerId,
+    pinnedParticipants,
+    presenterId,
+    localScreenShareOn,
+  ]);
+
+  return (
+    <MemoizedParticipantGrid
+      participantIds={participantIds}
+      isPresenting={isPresenting}
+    />
+  );
+}
+
+const MemorizedParticipantView = React.memo(
+  ParticipantsViewer,
+  (prevProps, nextProps) => {
+    return prevProps.isPresenting === nextProps.isPresenting;
+  }
+);
+
+export default MemorizedParticipantView;
